79aa729cc3bd32c1797f00175cd5f55ecfa6aec1
Roman Abaghayn <84144854+777ARM777@users.noreply.github.com>
2024-05-04 11:36:46 +0400

    Delete student.py

--- student.py
+++
@@ -1,7 +0,0 @@
-def get_mean(values):
-    return sum(values) / len(values)
-
-# The list of numbers
-# num_lst = [3, 6, 5, 7, 9, 1]
-# The function call
-# print(f'{get_mean(num_lst):.2f}')  # Print the result
